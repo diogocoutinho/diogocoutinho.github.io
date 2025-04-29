08b05ab40f76a1f21a22b95b2247d4f27e8871db
Diogo Coutinho <diogo.coutinho@odgo.com.br>
2025-04-29 11:26:02 -0300

    Refactor Next.js configuration by removing custom webpack setup for CSS and updating PostCSS configuration to use the new Tailwind CSS plugin format. Enhanced global styles with Tailwind directives for base, components, and utilities.

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ["style-loader", "css-loader", "postcss-loader"],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
